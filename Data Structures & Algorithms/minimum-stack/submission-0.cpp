f98ab031450010889eb15b21d749828ecb625b23
kartik goel <goyalkartik773@gmail.com>
2026-06-17 04:28:03 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<int> st;
+    MinStack() {   
+    }
+    void push(int val) {
+        st.push(val);
+    }
+    void pop() {
+        st.pop();
+    }
+    int top() {
+        return st.top();
+    }
+    int getMin() {
+        stack<int> temp;
+        temp.push(st.top());
+        st.pop();
+        while(st.size()!=0 && temp.top()>st.top()){
+            temp.push(st.top());
+            st.pop();
+        }
+        int ans = temp.top();
+        while(temp.size()!=0){
+            st.push(temp.top());
+            temp.pop();
+        }
+        return ans;
+    }
+};
